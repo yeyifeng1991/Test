5ec506dcd01e79d9ab086453eea24e0034acb41d
叶义峰 <912907352@qq.com>
2021-04-27 16:12:13 +0800

    111

--- Test/AppDelegate.m
+++ Test/AppDelegate.m
@@ -16,6 +16,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+//     启动函数
     return YES;
 }
 
