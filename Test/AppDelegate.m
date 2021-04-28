f9ba25a9870d90614da4accd2f4a3ded5fdfd66b
叶义峰 <912907352@qq.com>
2021-04-28 09:47:38 +0800

    Revert "Revert "Revert "dev启动函数"""
    This reverts commit 22625af933b903fd800add53903309574769629c.

--- Test/AppDelegate.m
+++ Test/AppDelegate.m
@@ -17,7 +17,6 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
 //     启动函数
-//     dev启动函数
     return YES;
 }
 
